8e4a2218c32704b9fced05d24c4067a37513da77
Sean McCarthy <sean.mccarthy3223@gmail.com>
2025-06-13 11:09:52 -0500

    Test commit

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -17,7 +17,6 @@ const Contact = () => {
       <ContactHero />
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
         <div className="grid lg:grid-cols-2 gap-12">
-          <ContactForm />
           <ContactInfo />
         </div>
       </div>
